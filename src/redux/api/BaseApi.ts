afd506b27aa09b8bd8ea2aed55d722416e228b64
devhasanmia <dev.hasanmia@gmail.com>
2024-11-22 16:41:23 +0600

    Url Updated

--- src/redux/api/BaseApi.ts
+++ src/redux/api/BaseApi.ts
@@ -4,7 +4,7 @@ import { RootState } from "../store";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "portfolio-server-one-olive.vercel.app/api/v1",
+    baseUrl: "https://portfolio-server-one-olive.vercel.app/api/v1",
     prepareHeaders(headers, { getState }) {
       const token = (getState() as RootState)?.auth?.token;
       if (token) {
